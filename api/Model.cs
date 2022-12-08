5e04982f0ded11d428127eb0d4f349c8be4a2272
emil <emil@emil>
2022-12-08 15:24:06 +0100

    Several modifications to REST API, and also fixed type in endpoints.md

--- api/Model.cs
+++ api/Model.cs
@@ -1,68 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace InventoryAPI.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class InventoryController : ControllerBase
-    {
-        private readonly InventoryContext _context;
-        public InventoryController(InventoryContext context)
-        {
-            _context = context;
-        }
-
-        // GET api/inventory/get/all
-        [HttpGet("get/all")]
-        public async Task<ActionResult<IEnumerable<InventoryItem>>> GetAllInventory()
-        {
-            return await _context.Inventory.ToListAsync();
-        }
-
-        // GET api/inventory/get/{amount}
-        [HttpGet("get/{amount}")]
-        public async Task<ActionResult<IEnumerable<InventoryItem>>> GetInventoryByAmount(int amount)
-        {
-            return await _context.Inventory
-                .Where(i => i.Amount == amount)
-                .ToListAsync();
-        }
-
-        // GET api/inventory/get/{query}
-        [HttpGet("get/{query}")]
-        public async Task<ActionResult<IEnumerable<InventoryItem>>> GetInventoryByQuery(string query)
-        {
-            return await _context.Inventory
-                .Where(i => i.FtzNumber.Contains(query) ||
-                            i.AltName.Contains(query) ||
-                            i.Description.Contains(query) ||
-                            i.Section.Contains(query) ||
-                            i.SectionName.Contains(query) ||
-                            i.PlacementNotes.Contains(query))
-                .ToListAsync();
-        }
-
-        // PUT api/inventory/put/add
-        [HttpPut("put/add")]
-        public async Task<IActionResult> PutAddInventory(InventoryItem item)
-        {
-            if (item.Amount == 0)
-            {
-                item.Amount = 1;
-            }
-
-            _context.Inventory.Add(item);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(item.FtzNumber), new { id = item.AltName });
-        }
-    }
-}
+namespace InventoryAPI.Model;
 
 public class InventoryItem
 {
@@ -79,8 +17,8 @@ public class InventoryItem
 
 public class InventoryContext : DbContext
 {
-    public InventoryContext(DbContextOptions<InventoryContext> options)
-    : base(options)
-    { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql("server=localhost;database=db_testing;user=root", new MariaDbServerVersion(new Version(10, 9, 3)));
+
     public DbSet<InventoryItem> Inventory { get; set; }
 }
