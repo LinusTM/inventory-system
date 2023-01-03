ab2249b1bc9648f673b4a90d6fe6b12746a29a6f
emils0 <89942504+emils0@users.noreply.github.com>
2023-01-03 10:49:06 +0100

    Made some changes to the table in the frontend.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -123,14 +123,16 @@
 	const className = {
 		table: 'table',
 		header: 'table-header',
-		tbody: 'table-body'
+		tbody: 'table-body',
+		td: 'table-td',
+		th: 'table-th'
 	};
 </script>
 
 <h1>Søg på reservedel</h1>
 
 <div id="container">
-	<Grid {data} {className} search={true} autoWidth={true} fixedHeader={false} resizable={true} />
+	<Grid {data} {className} search={true} autoWidth={true} sort={false} />
 </div>
 
 <style lang="scss">
@@ -138,7 +140,12 @@
 
 	:global(.table) {
 		border: solid var(--background-dark);
+		border-radius: 20px;
 		margin: 0;
+	}
+
+	:global(.table-td, .table-th) {
+		padding: 0;
 	}
 
 	:global(.table-body > tr) {
@@ -160,64 +167,13 @@
 		transition: color 0.3s linear;
 	}
 
+	:global(.table-body > th) {
+		text-align: center;
+	}
+
 	#container {
 		@media screen and (max-width: 800px) {
 			margin: 0 5%;
 		}
-	}
-
-	#result {
-		display: flex;
-		margin-bottom: 1rem;
-		background-color: var(--background-dark);
-		color: var(--text-light);
-		border-color: var(--text);
-		border-width: 3px;
-		box-shadow: 0px 12px 13px -16px rgba(0, 0, 0, 0.5);
-	}
-
-	img {
-		max-width: 10rem;
-		margin-right: 1rem;
-	}
-
-	h3,
-	p {
-		margin: 0;
-	}
-
-	form {
-		color: var(--text);
-		width: 100%;
-		height: 2.75em;
-		border-radius: 2px;
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		background-color: var(--background-medium);
-		border-bottom: 2px solid var(--text);
-		margin: auto;
-		margin-bottom: 4.5em;
-	}
-
-	input {
-		all: unset;
-		height: 100%;
-		width: 100%;
-		padding: 6px 10px;
-	}
-
-	button {
-		all: unset;
-		cursor: pointer;
-		width: 44px;
-		height: 44px;
-		color: var(--text);
-		font-weight: bold;
-		padding-right: 1.2em;
-	}
-
-	::placeholder {
-		opacity: 0.7;
 	}
 </style>
