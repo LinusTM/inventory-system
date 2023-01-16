f558a0fadcc7cb9c8ec498fdff80714b11cf0c25
emils0 <89942504+emils0@users.noreply.github.com>
2023-01-16 14:55:18 +0100

    Separated code out into separate file.

---
+++ frontend/src/lib/shared/ts/entry.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker';
+
+// Defines the "Entry" class, so that we can easily couple the data together
+export class Entry {
+	ftzNumber: string;
+	altName: string;
+	description: string;
+	section: string;
+	sectionName: string;
+	placementNotes: string;
+	amount: number;
+	picture: string;
+
+	// The constructor will generate random values if no parameters are given.
+	constructor() {
+		this.ftzNumber = faker.random.alpha(1) + faker.random.numeric(3);
+		this.altName = faker.commerce.productAdjective() + faker.commerce.product();
+		this.description = faker.commerce.productName();
+		this.section = faker.random.alphaNumeric(2);
+		this.sectionName = faker.random.words(2);
+		this.placementNotes = faker.lorem.words(4);
+		this.amount = Math.floor(Math.random() * 500);
+		this.picture = faker.image.image(420, 420, true);
+	}
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,33 +1,9 @@
 <script lang="ts">
+	import { Entry } from '$lib/shared/ts/entry';
 	import Grid from 'gridjs-svelte';
 	import Modal from 'svelte-simple-modal';
-	import { faker } from '@faker-js/faker';
 
-	// Defines the "Entry" class, so that we can easily couple the data together
-	class Entry {
-		ftzNumber: string;
-		altName: string;
-		description: string;
-		section: string;
-		sectionName: string;
-		placementNotes: string;
-		amount: number;
-		picture: string;
-
-		// The constructor will generate random values if no parameters are given.
-		constructor() {
-			this.ftzNumber = faker.random.alpha(1) + faker.random.numeric(3);
-			this.altName = faker.commerce.productAdjective() + faker.commerce.product();
-			this.description = faker.commerce.productName();
-			this.section = faker.random.alphaNumeric(2);
-			this.sectionName = faker.random.words(2);
-			this.placementNotes = faker.lorem.words(4);
-			this.amount = Math.floor(Math.random() * 500);
-			this.picture = faker.image.image(420, 420, true);
-		}
-	}
-
-	// Creates an array with a length of 1000, filled with random data.
+	// Creates an array with a length of 1000, filled with random entries.
 	// Currently used for testing purposes.
 	const data: any = Array.from({ length: 1000 }, () => new Entry()),
 		// We are assigning parts of the table to classes, so that we can modify the styling.
